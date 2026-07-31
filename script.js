5e18d2f9af1c174ac6136870e756b0ecf438e06e
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-01 01:18:19 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function copyText(button){
+let text=button.parentElement.querySelector("p").innerText;
+navigator.clipboard.writeText(text);
+button.innerText="کپی شد ✓";
+setTimeout(()=>button.innerText="کپی متن",2000);
+}
