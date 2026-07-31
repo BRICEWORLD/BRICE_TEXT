7f932c279b026a28c1210d1b0f7d95a87c74cf39
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-01 01:17:14 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function copyText(button){
-let text = button.parentElement.querySelector("p").innerText;
-navigator.clipboard.writeText(text);
-button.innerText="کپی شد ✓";
-setTimeout(()=>{
-button.innerText="کپی متن";
-},2000);
-}
